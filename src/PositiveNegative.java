834c420a793eedb5b6f709c68ff4229b972d279c
Victoria Rotari <rotarivictoria@yahoo.com>
2017-10-15 19:54:26 -0700

    Updated the file

--- src/PositiveNegative.java
+++ src/PositiveNegative.java
@@ -1,11 +1,26 @@
-public class PositiveNegative {
+
 
 	/*
 	 * define a program that finds if a number is positive or negative.
 	 * 
 	 * The program should print positive if the number is positive, negative if it is negative
 	 */
-	public static void main(String args[]){
-		
+	
+	public class PositiveNegative {
+
+		public static void main(String[] args) {
+			int x = - 7;
+					
+			if (x > 0) {
+				System.out.println("positive");
+			}
+			  else if ( x < 0) {
+				System.out.println("negative");
+			}
+			  else {
+				  System.out.println("zero");
+			  }
+
+		}
 	}
-}
+	
