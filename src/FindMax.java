5018556dfe3fc1ac29279f5fe42579cf56d82360
Victoria Rotari <rotarivictoria@yahoo.com>
2017-10-15 19:44:38 -0700

    Adding FindMax to git

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,15 +1,36 @@
 
 public class FindMax {
 
-	/*
-	 * Define a program that finds the maximum number when 2 numbers are given.
-	 */
-	public static void main(String[] args) {
-		if (5<6) {
-			System.out.println("true");
-		}
-		// TODO Auto-generated method stub
+	
 
+				/*
+				 * Define a program that finds the maximum number when 2 numbers are given.
+				 */
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+					int x = 20;
+					int y = 30;
+					int max = 0;
+					
+					
+					if (x > y) {
+			            max = x;
+			            System.out.println(max);
+					}
+					else if (y > x) {
+						max =y;
+						System.out.println(max);
+						
+				    }
+					else {
+						System.out.println("Equal");
+					}
+				
+				
+				}
+
+			
+		
 	}
 
-}
+
