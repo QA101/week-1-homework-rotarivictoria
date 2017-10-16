f9e780a38785a887c7fe270422ae5b90fe97853f
Victoria Rotari <rotarivictoria@yahoo.com>
2017-10-15 20:07:54 -0700

    adding the programm to the git

--- src/DivisibleProgram.java
+++ src/DivisibleProgram.java
@@ -1,17 +1,30 @@
 
-public class DivisibleProgram {
 
-	/*
-	 * Define a program that determines whether a number x is divisible by a number y.
-	 * 
-	 * If x is divisble by y, the program will print TRUE otherwise FALSE
-	 * 
-	 * Such as:
-	 * x -> 10
-	 * y -> 5
-	 * 10 divided by 5 is 2 with 0 remainder. The program would print true
-	 */
-	public static void main(String args[]){
-		
+	 
+	public class DivisibleProgram {
+
+		/*
+		 * Define a program that determines whether a number x is divisible by a number y.
+		 * 
+		 * If x is divisble by y, the program will print TRUE otherwise FALSE
+		 * 
+		 * Such as:
+		 * x -> 10
+		 * y -> 5
+		 * 10 divided by 5 is 2 with 0 remainder. The program would print true
+		 */
+		public static void main(String args[]){
+			
+			int x = 10;
+			int y = 5;
+			
+			if (x%y == 0) {
+				System.out.println("true");
+			}
+			else {
+				System.out.println("false");
+			}
+			
+		}
 	}
-}
+
