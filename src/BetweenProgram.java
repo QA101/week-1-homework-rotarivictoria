0c5e075e1c951dc21bacad9efb8f419504683c57
rotarivictoria <rotarivictoria@yahoo.com>
2017-10-15 20:24:36 -0700

    Add files via upload

--- src/BetweenProgram.java
+++ src/BetweenProgram.java
@@ -1,20 +1,33 @@
 
 public class BetweenProgram {
 
-	/*
-	 * Write a program that determine if a number x is between 2 numbers y and z
-	 * if x is between y and z, print TRUE otherwise print FALSE
-	 * 
-	 * example:
-	 * x = 150
-	 * y = 100
-	 * z = 200
-	 * should print TRUE
-	 * 
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	
+
+		/*
+		 * Write a program that determine if a number x is between 2 numbers y and z
+		 * if x is between y and z, print TRUE otherwise print FALSE
+		 * 
+		 * example:
+		 * x = 150
+		 * y = 100
+		 * z = 200
+		 * should print TRUE
+		 * 
+		 */
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int x = 150;
+			int y = 100;
+			int z = 200;
+			
+			if (x > y && z > x) {
+				System.out.println("true");
+			}
+			else {
+				System.out.println("false");
+			}
+			
+		}
 
 	}
-
-}
