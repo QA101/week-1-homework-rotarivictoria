f805060d4f87331eeba424acc091e301306894ea
Victoria Rotari <rotarivictoria@yahoo.com>
2017-10-15 19:26:24 -0700

    Adding my first code

--- src/FindMax.java
+++ src/FindMax.java
@@ -5,6 +5,9 @@ public class FindMax {
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
 	public static void main(String[] args) {
+		if (5<6) {
+			System.out.println("true");
+		}
 		// TODO Auto-generated method stub
 
 	}
